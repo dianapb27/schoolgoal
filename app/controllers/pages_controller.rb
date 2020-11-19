6b26ce22cd10ba87c05ebc4e6f651aaa52c5744a
Sean OReilly <soreilly424@gmail.com>
2020-11-19 17:54:44 -0500

    I have changed the route so that in the index page you will find the results of the search query

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,5 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    if params[:query].present?
-       @courses = Course.search_by_title_and_description_and_category(params[:query])
-    else
-      @courses = Course.all
-    end
   end
 end
